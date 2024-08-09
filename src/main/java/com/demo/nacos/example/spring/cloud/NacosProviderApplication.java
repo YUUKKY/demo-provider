9350a8e64aa89ca482dd834c89071c36973c1dbc
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2024-08-09 13:24:53 +0800

    Update NacosProviderApplication.java

--- src/main/java/com/demo/nacos/example/spring/cloud/NacosProviderApplication.java
+++ src/main/java/com/demo/nacos/example/spring/cloud/NacosProviderApplication.java
@@ -25,5 +25,34 @@ public class NacosProviderApplication {
 		public String echo(@PathVariable String string) {
 			return "Hello CAE: " + string;
 		}
+
+		public void writeToDB() {
+			Connection conn = null;
+			String ip = System.getenv("RDS_ADDRESS");
+			String port = System.getenv("RDS_PORT");
+			String dbName =  System.getenv("RDS_DB_NAME");
+		        String url = "jdbc:postgresql://ip:port/dbName?sslmode=verify-ca&sslrootcert=/home/Ruby/ca.pem";
+		        String userName = System.getenv("RDS_USER_NAME");
+		        String password = System.getenv("RDS_PASSWORD");
+
+		        try {
+			  Class.forName("org.postgresql.Driver");
+			  conn = DriverManager.getConnection(url, userName, password);
+			  System.out.println("Database connected");
+
+			  Statement stmt = conn.createStatement();
+			  ResultSet rs = stmt.executeQuery("INSERT * FROM mytable WHERE columnfoo = 500");
+			  while (rs.next()) {
+			    System.out.println(rs.getString(1));
+			  }
+
+			  rs.close();
+			  stmt.close();
+			  conn.close();
+		        } catch (Exception e) {
+			  e.printStackTrace();
+			  System.out.println("Test failed");
+		        }
+		}
 	}
 }
