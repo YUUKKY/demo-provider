a08724d8cda78b63077564366aa67eb3cc81b11b
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2024-08-09 15:19:11 +0800

    Update NacosProviderApplication.java

--- src/main/java/com/demo/nacos/example/spring/cloud/NacosProviderApplication.java
+++ src/main/java/com/demo/nacos/example/spring/cloud/NacosProviderApplication.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin
 public class NacosProviderApplication {
 
 	public static void main(String[] args) {
